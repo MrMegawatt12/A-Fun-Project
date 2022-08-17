469317612b90b566754e5fe8a6afbae1be8e2e7c
MrMegawatt12 <111433215+MrMegawatt12@users.noreply.github.com>
2022-08-17 21:50:38 +0800

    Update upgrades.js

--- js/upgrades.js
+++ js/upgrades.js
@@ -28,10 +28,10 @@ const UPGS = {
                 desc: `Increase Grass gain by <b class="green">10</b> per level.<br>This effect is <b class="green">doubled</b> every <b class="yellow">25</b> levels.`,
 
                 res: "grass",
-                icon: ['Curr/AntiGrass'],
+                icon: ['Curr/Planet.png'],
                 
                 cost: i => Decimal.pow(1,i).mul(10).ceil(),
-                bulk: i => i.div(10).max(1).log(1.000000000000001).floor().toNumber()+10,
+                bulk: i => i.div(10).max(1).log(1.01).floor().toNumber()+10,
 
                 effect(i) {
                     let x = Decimal.pow(2,Math.floor(i/25)).mul(i+10)
