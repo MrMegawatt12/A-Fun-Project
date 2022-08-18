64dabf7b4be93b2fac46ce96296b1b2a516451c9
MrMegawatt12 <111433215+MrMegawatt12@users.noreply.github.com>
2022-08-18 08:47:11 +0800

    Update upgrades.js

--- js/upgrades.js
+++ js/upgrades.js
@@ -31,7 +31,7 @@ const UPGS = {
                 icon: ['Curr/Grass'],
                 
                 cost: i => Decimal.pow(1,i).mul(10).ceil(),
-                bulk: i => i.div(10).max(1).log(1.1).floor().toNumber()+1,
+                bulk: i => i.div(10).max(1).log(1.01).floor().toNumber()+1,
 
                 effect(i) {
                     let x = Decimal.pow(2,Math.floor(i/25)).mul(i+1)
